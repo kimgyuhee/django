214d17abe114ed138b817eb28ed0408e0c57b3ab
개발하는 김곰돌ᕕʕ •ₒ• ʔ୨ <rbgmll10331@gmail.com>
2022-05-13 10:11:24 +0900

    FBV order by

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
+
+# def index(request) :
+#     return render(
+#         request,
+#         'blog/index.html',
+#     )
+
 def index(request) :
+
+    posts = Post.objects.all().order_by('-pk')
+
     return render(
         request,
         'blog/index.html',
+        {
+            'posts' : posts
+        }
     )
